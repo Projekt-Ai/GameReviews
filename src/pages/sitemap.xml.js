0d030369445c8717dbb532bb19407490dae83fb5
Christian Thomas <n01540490@unf.edu>
2026-03-17 03:24:48 -0400

    Add JSON-LD structured data to BossFeatureLayout and update sitemap generation

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -3,12 +3,19 @@ import { getVisibleBossFeatures, getVisibleReviews, siteConfig } from "../lib/co
 export const GET = async () => {
   const [reviews, bossfeatures] = await Promise.all([getVisibleReviews(), getVisibleBossFeatures()]);
   const staticRoutes = ["/", "/about/", "/search/", "/reviews/", "/bossfeatures/", "/rss.xml", "/404.html"];
-  const dynamicRoutes = [
-    ...reviews.map((post) => `/reviews/${post.slug}/`),
-    ...bossfeatures.map((post) => `/bossfeatures/${post.slug}/`),
-  ];
-  const routes = [...new Set([...staticRoutes, ...dynamicRoutes])];
-  const urls = routes.map((route) => `  <url><loc>${new URL(route, siteConfig.url).toString()}</loc></url>`).join("\n");
+
+  const toUrl = (route, lastmod) => {
+    const loc = new URL(route, siteConfig.url).toString();
+    const lastmodTag = lastmod ? `<lastmod>${lastmod.toISOString().split("T")[0]}</lastmod>` : "";
+    return `  <url><loc>${loc}</loc>${lastmodTag}</url>`;
+  };
+
+  const urls = [
+    ...staticRoutes.map((route) => toUrl(route, null)),
+    ...reviews.map((post) => toUrl(`/reviews/${post.slug}/`, post.data.updatedDate ?? post.data.pubDate)),
+    ...bossfeatures.map((post) => toUrl(`/bossfeatures/${post.slug}/`, post.data.updatedDate ?? post.data.pubDate)),
+  ].join("\n");
+
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
